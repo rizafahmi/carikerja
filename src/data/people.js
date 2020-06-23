e392bfb9a113b33ba51564678401091fafa445ca
Adzanny <adzannyaa@gmail.com>
2020-06-23 11:22:52 +0700

    Update People.js
    add profile

--- src/data/people.js
+++ src/data/people.js
@@ -131,5 +131,17 @@ module.exports = [{
         },
         tech_stack: ['Laravel', 'Codeigniter', 'Angular', 'Ionic'],
         hired: false
+    },
+    {
+        name: 'Adzanny Akbar',
+        status: 'Fulltime, Remote',
+        role: 'Full Stack Developer',
+        location: 'Jakarta',
+        social_media: {
+            Linkedin: 'https://www.linkedin.com/in/adzannyaa/',
+            Github: 'https://gitlab.com/zanisis'
+        },
+        tech_stack: ['React', 'React Native', 'Flutter', 'NodeJS'],
+        hired: true
     }
 ];
