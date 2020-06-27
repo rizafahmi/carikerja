b18fba8fb233ff60a5648623717b74559e89b242
Drzaln <drzaln@null.net>
2020-06-27 15:23:47 +0700

    Hiring page sort alphabetically

--- src/routes/hiring.svelte
+++ src/routes/hiring.svelte
@@ -1,12 +1,30 @@
 <script>
-  import employers from '../data/employer.js';
+  import employers from "../data/employer.js";
+
+  employers.sort((a, b) => {
+    const nameA = a.name.replace(/\s/g, "").toUpperCase();
+    const nameB = b.name.replace(/\s/g, "").toUpperCase();
+
+    if (nameA < nameB) {
+      return -1;
+    } else if (nameA > nameB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 </script>
 
 <svelte:head>
-  <title>Untukmu yang terdampak, temukan beberapa perusahaan yang masih membuka lowongan</title>
+  <title>
+    Untukmu yang terdampak, temukan beberapa perusahaan yang masih membuka
+    lowongan
+  </title>
 </svelte:head>
 
-<h1>Untukmu yang terdampak, temukan beberapa perusahaan yang masih membuka lowongan:
+<h1>
+  Untukmu yang terdampak, temukan beberapa perusahaan yang masih membuka
+  lowongan:
 </h1>
 
 <ul>
@@ -23,5 +41,16 @@
 <hr />
 <h4>Ingin perusahaanmu ada di daftar kami?</h4>
 <p>
-  <a href="https://github.com/rizafahmi/carikerja" target="_blank">Clone repositori ini</a> dan tambahkan data baru di <code>src/data/employer.js</code>. Bisa juga edit langsung via <a href="https://github.com/rizafahmi/carikerja/edit/master/src/data/employer.js" target="_blank">Github</a>.
+  <a href="https://github.com/rizafahmi/carikerja" target="_blank">
+    Clone repositori ini
+  </a>
+  dan tambahkan data baru di
+  <code>src/data/employer.js</code>
+  . Bisa juga edit langsung via
+  <a
+    href="https://github.com/rizafahmi/carikerja/edit/master/src/data/employer.js"
+    target="_blank">
+    Github
+  </a>
+  .
 </p>
