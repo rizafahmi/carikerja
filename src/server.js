ead6eaac28babf29703a5283b47c1b22e102ba2d
Riza Fahmi <rizafahmi@gmail.com>
2021-02-16 13:44:36 +0700

    Fixing with eslint and prettier

--- src/server.js
+++ src/server.js
@@ -3,15 +3,11 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
-const { PORT, NODE_ENV } = process.env;
+const {PORT, NODE_ENV} = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(compression({threshold: 0}), sirv('static', {dev}), sapper.middleware())
+  .listen(PORT, (err) => {
+    if (err) console.log('error', err);
+  });
